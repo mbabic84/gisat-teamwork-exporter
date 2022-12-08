30f88e7788e9a277b6285d7fe6bfefd76f19636a
Martin Babič <kralicinora@gmail.com>
2022-12-08 15:59:19 +0100

    init

---
+++ index.mjs
@@ -0,0 +1,166 @@
+import axios from 'axios';
+import moment from 'moment';
+import fs from 'fs';
+
+const baseApiUrl = `https://gisat.teamwork.com`;
+
+async function getUserDetails({ apiKey }) {
+    const response = await axios({
+        auth: {
+            username: apiKey,
+            password: `authByApiKey`
+        },
+        url: `${baseApiUrl}/me.json`
+    })
+
+    return response.data;
+}
+
+function getUserId({ userDetails }) {
+    return userDetails.person.id;
+}
+
+function getTimeInterval({month}) {
+    if (month) {
+        return {
+            from: moment().month(Number(month)-1).startOf("month").format("YYYYMMDD"),
+            to: moment().month(Number(month)-1).endOf("month").format("YYYYMMDD")
+        }
+    } else {
+        return {
+            from: moment().subtract(1, "month").startOf("month").format("YYYYMMDD"),
+            to: moment().subtract(1, "month").endOf("month").format("YYYYMMDD")
+        }
+    }
+}
+
+async function getLoggedTime({ apiKey, userId, timeInterval, page = 0, entries = [] }) {
+    const response = await axios({
+        auth: {
+            username: apiKey,
+            password: `authByApiKey`
+        },
+        url: `${baseApiUrl}/time_entries.json`,
+        params: {
+            userId,
+            fromDate: timeInterval.from,
+            toDate: timeInterval.to,
+            page
+        }
+    });
+
+    const currentPage = Number(response.headers['x-page']);
+    const totalPages = Number(response.headers['x-pages']);
+
+    entries = entries.concat(response.data['time-entries']);
+
+    if (currentPage < totalPages) {
+        return await getLoggedTime({ apiKey, userId, timeInterval, page: currentPage + 1, entries })
+    } else {
+        return entries;
+    }
+}
+
+async function getTotals({ loggedTime, timeInterval }) {
+    if (!loggedTime.length) {
+        throw new Error("No logged time");
+    }
+
+    const totals = {
+        days: {},
+        summary: {},
+        user: `${loggedTime[0]['person-first-name']} ${loggedTime[0]['person-last-name']}`,
+        total: 0,
+        from: timeInterval.from,
+        to: timeInterval.to
+    };
+
+    for (const entry of loggedTime) {
+        const project = entry['project-name'];
+        const task = `${entry.parentTaskName ? `${entry.parentTaskName} >> ` : ``}${entry['todo-item-name']}`;
+        const time = Number(entry.hoursDecimal);
+
+        if (!totals.summary[project]) {
+            totals.summary[project] = {
+                tasks: {
+                    [task]: 0
+                },
+                total: 0
+            }
+        }
+
+        if (!totals.summary[project].tasks[task]) {
+            totals.summary[project].tasks[task] = 0;
+        }
+
+        const day = moment(entry.dateUserPerspective).format("DD.MM. - dddd");
+
+        if (!totals.days[day]) {
+            totals.days[day] = {
+                ...totals.days[day],
+                projects: {
+                    [project]: {
+                        tasks: {
+                            [task]: 0
+                        },
+                        total: 0
+                    }
+                },
+                total: 0
+            }
+        }
+
+        if (!totals.days[day].projects[project]) {
+            totals.days[day].projects[project] = {
+                tasks: {
+                    [task]: 0
+                },
+                total: 0
+            }
+        }
+
+        if (!totals.days[day].projects[project].tasks[task]) {
+            totals.days[day].projects[project].tasks[task] = 0
+        }
+
+        totals.summary[project].tasks[task] += time;
+        totals.summary[project].total += time;
+        totals.days[day].projects[project].tasks[task] += time;
+        totals.days[day].projects[project].total += time;
+        totals.days[day].total += time;
+        totals.total += time;
+    }
+
+    return totals;
+}
+
+async function exportTotals({ timeInterval, totals }) {
+    const totalsStr = JSON.stringify(totals, null, 2);
+    fs.writeFileSync(`TeamWork_Time_${timeInterval.from}_${timeInterval.to}.json`, totalsStr);
+    console.log(totalsStr);
+}
+
+async function run({ apiKey, month }) {
+    const userDetails = await getUserDetails({ apiKey });
+    const userId = getUserId({ userDetails });
+    const timeInterval = getTimeInterval({month});
+    const loggedTime = await getLoggedTime({ apiKey, userId, timeInterval });
+    const totals = await getTotals({ loggedTime, timeInterval });
+
+    await exportTotals({ timeInterval, totals });
+}
+
+async function init() {
+    try {
+        const [apiKey, month] = process.argv.slice(2);
+        if (apiKey) {
+            await run({ apiKey, month });
+        } else {
+            throw new Error(`Missing API Key!`);
+        }
+    } catch (e) {
+        console.log(e);
+    }
+}
+
+init();
---
+++ readme.md
@@ -0,0 +1,7 @@
+Prepare: npm i
+
+Run: node index.mjs <apiKey> [number of month 1-12]
+
+You can get your apiKey here: TeamWork -> User -> Edit my details -> Api & Mobile
+
+Code will export totals of logged times for previous month (by default, can be specified). It will print results to console formated as JSON and save it to file as well.
